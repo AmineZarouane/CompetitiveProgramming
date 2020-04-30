f41f9e422f8b82b2cf527ba9e24a93a80912ce7c
AmineZarouane <aminezarouane@gmail.com>
2020-04-30 11:59:08 +0100

    Completed LeetCode 30-Day LeetCoding Challenge

---
+++ Leetcode/30-Day LeetCoding Challenge/30 - Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool leaf(TreeNode* root){
+        return !(root->left && root->right)
+    }
+    bool dfs(TreeNode* root, vector<int>& arr,int idx = 0){
+        if (!root || root->val != arr[idx])
+            return false;
+        if (idx + 1 == (int)arr.size())
+            return !root->left && !root->right;
+        return dfs(root->left, arr, idx + 1) || dfs(root->right,arr,idx + 1);
+    }
+    bool isValidSequence(TreeNode* root, vector<int>& arr) {
+        return dfs(root,arr);
+    }
+};
